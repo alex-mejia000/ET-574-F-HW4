52857d5f47298913288b05ad6654e9fc1902562c
alex-mejia000 <ALEXANDER.MEJIA20@student.qcc.cuny.edu>
2025-11-02 00:03:00 -0400

    Finished the first part for main.py

--- main.py
+++ main.py
@@ -28,7 +28,34 @@ def play_round(self):
             "total": total,
             "result": result
         }
-    def evaluate_roll(self, total):
+    
+def evaluate_roll(self, total):
         if total in [7, 11]:
             return "Win"
-        elif total in [2,3,12]
+        elif total in [2,3,12]:
+            return "Lose"
+        else:
+            return 
+        
+def main():
+    game = DiceGame()
+    while True:
+        print("\nDice Rolling Simulator")
+        print("1. Spin and Bet")
+        print("2. Be a quitter before winning")
+
+        choice = input("Choose an option:")
+
+        if choice == "1":
+            result = game.play_round()
+            print(f"\n You rolled: {result['die1']} and {result['die2']}")
+            print(f"Total: {result['total']}")
+            print(f"Outcome: {result['result']}")
+        elif choice == "2":
+             print("Okay, come back soon")
+             break
+        else:
+             print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":             
+    main()
